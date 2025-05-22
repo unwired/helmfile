cc93a0fc92dd2011087739e0067e0c9e787bbaf4
Niklas Ott <niklas.ott@unwired.at>
2025-05-22 10:26:36 +0200

    chore: Emit "cleanup" events later to match the behavior in "apply"
    This changes the behaviour of the cleanup event during sync to be
    triggered right before the function exits and matches the behaviour of
    apply
    
    Signed-off-by: Niklas Ott <niklas.ott@unwired.at>

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2172,13 +2172,6 @@ func (a *App) SyncRun(r *Run, c SyncConfigProvider) (bool, []error) {
 		}
 	}
 
-	for id := range releasesWithNoChange {
-		r := releasesWithNoChange[id]
-		if _, err := st.TriggerCleanupEvent(&r, "sync"); err != nil {
-			a.Logger.Warnf("warn: %v\n", err)
-		}
-	}
-
 	names := []string{}
 	for _, r := range releasesToUpdate {
 		names = append(names, fmt.Sprintf("  %s (%s) UPDATED", r.Name, r.Chart))
@@ -2279,6 +2272,13 @@ Do you really want to sync?
 	}
 
 	affectedReleases.DisplayAffectedReleases(c.Logger())
+
+	for id := range releasesWithNoChange {
+		r := releasesWithNoChange[id]
+		if _, err := st.TriggerCleanupEvent(&r, "sync"); err != nil {
+			a.Logger.Warnf("warn: %v\n", err)
+		}
+	}
 
 	return true, errs
 }
